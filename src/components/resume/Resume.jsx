3deaab43e1705ab7c337f160b9a602f06c257df8
Yindong Wang <wangyind@cip.ifi.lmu.de>
2021-09-20 12:37:14 +0200

    add html code for resume

---
+++ src/components/resume/Resume.jsx
@@ -0,0 +1,11 @@
+import "./cv.scss"
+import React, { Component } from "react";
+
+export default function Cv() {
+
+    return (
+        <div className="description">
+            This is my resume
+        </div>
+    );
+}
